cee589d1c042a9200dc4abf0cd8b5a9e265ba62e
Kevin Silva Alfonzo <kevin.silva@vallegrande.edu.pe>
2022-05-06 14:46:05 -0500

    Agregando datos

---
+++ Data/dbo.CLIENTE_Data.sql
@@ -0,0 +1,12 @@
+SET IDENTITY_INSERT [dbo].[CLIENTE] ON
+INSERT INTO [dbo].[CLIENTE] ([IDCLI], [NOMCLI], [APECLI], [DNICLI], [CELCLI], [DIRCLI], [ESTCLI]) VALUES (1, N'Pedro', N'Suarez Vertiz', N'47696801', N'987654321', N'Jr. Damián Tello', N'A')
+INSERT INTO [dbo].[CLIENTE] ([IDCLI], [NOMCLI], [APECLI], [DNICLI], [CELCLI], [DIRCLI], [ESTCLI]) VALUES (2, N'Hugo', N'Guaenizo Carguas', N'70203962', N'973904378', N'Urb. San Olivia', N'A')
+INSERT INTO [dbo].[CLIENTE] ([IDCLI], [NOMCLI], [APECLI], [DNICLI], [CELCLI], [DIRCLI], [ESTCLI]) VALUES (3, N'Paola', N'Naivares Sandoval', N'94297255', N'919487193', N'Av. Banda Norte', N'A')
+INSERT INTO [dbo].[CLIENTE] ([IDCLI], [NOMCLI], [APECLI], [DNICLI], [CELCLI], [DIRCLI], [ESTCLI]) VALUES (4, N'Xavier', N'Gutierrez Rivas', N'74452946', N'909487193', N'Urb. Zoe Armendáriz', N'A')
+INSERT INTO [dbo].[CLIENTE] ([IDCLI], [NOMCLI], [APECLI], [DNICLI], [CELCLI], [DIRCLI], [ESTCLI]) VALUES (5, N'Gonzalo', N'Aybas Sanchez', N'28450714', N'998025207', N'Jr. Victoria Ponce', N'A')
+INSERT INTO [dbo].[CLIENTE] ([IDCLI], [NOMCLI], [APECLI], [DNICLI], [CELCLI], [DIRCLI], [ESTCLI]) VALUES (6, N'Jesus', N'Chumpitaz del Aguila', N'75642087', N'930578388', N'Av. San Catalina', N'A')
+INSERT INTO [dbo].[CLIENTE] ([IDCLI], [NOMCLI], [APECLI], [DNICLI], [CELCLI], [DIRCLI], [ESTCLI]) VALUES (7, N'Alex', N'Quispe Chavez', N'82975710', N'986379792', N'Urb. Puerto Rafael', N'A')
+INSERT INTO [dbo].[CLIENTE] ([IDCLI], [NOMCLI], [APECLI], [DNICLI], [CELCLI], [DIRCLI], [ESTCLI]) VALUES (8, N'Roxana', N'Vivanco Palomino', N'91252324', N'944604516', N'Jr. Paula Uribe', N'A')
+INSERT INTO [dbo].[CLIENTE] ([IDCLI], [NOMCLI], [APECLI], [DNICLI], [CELCLI], [DIRCLI], [ESTCLI]) VALUES (9, N'Angel', N'Guerreo Reynoso', N'49357911', N'945573442', N'Av. Bneavides', N'A')
+INSERT INTO [dbo].[CLIENTE] ([IDCLI], [NOMCLI], [APECLI], [DNICLI], [CELCLI], [DIRCLI], [ESTCLI]) VALUES (10, N'Helen', N'García Saldaña', N'18797912', N'971629064', N'Jr. Los Girasoles', N'A')
+SET IDENTITY_INSERT [dbo].[CLIENTE] OFF
---
+++ Data/dbo.PRODUCTO_Data.sql
@@ -0,0 +1,12 @@
+SET IDENTITY_INSERT [dbo].[PRODUCTO] ON
+INSERT INTO [dbo].[PRODUCTO] ([IDPROD], [NOMPROD], [DESCPROD], [MARPROD], [PREPROD]) VALUES (1, N'Zeus Collar', N'Collar para perros', N'Zeus', 5.00)
+INSERT INTO [dbo].[PRODUCTO] ([IDPROD], [NOMPROD], [DESCPROD], [MARPROD], [PREPROD]) VALUES (2, N'Cama Catapet', N'Cama para gatos', N'CatPet', 10.99)
+INSERT INTO [dbo].[PRODUCTO] ([IDPROD], [NOMPROD], [DESCPROD], [MARPROD], [PREPROD]) VALUES (3, N'Optimum', N'Alimento para perros', N'Optimus', 5.50)
+INSERT INTO [dbo].[PRODUCTO] ([IDPROD], [NOMPROD], [DESCPROD], [MARPROD], [PREPROD]) VALUES (4, N'DogSparkle', N'Crema dental para perros', N'Spaik', 6.30)
+INSERT INTO [dbo].[PRODUCTO] ([IDPROD], [NOMPROD], [DESCPROD], [MARPROD], [PREPROD]) VALUES (5, N'CaLove', N'Ratón de juguete para gatos', N'Play', 5.10)
+INSERT INTO [dbo].[PRODUCTO] ([IDPROD], [NOMPROD], [DESCPROD], [MARPROD], [PREPROD]) VALUES (6, N'Nogal', N'Cama para gatos', N'Catit Hesper', 239.99)
+INSERT INTO [dbo].[PRODUCTO] ([IDPROD], [NOMPROD], [DESCPROD], [MARPROD], [PREPROD]) VALUES (7, N'Pala Sanitaria', N'Peine para gatos', N'Granulos', 4.91)
+INSERT INTO [dbo].[PRODUCTO] ([IDPROD], [NOMPROD], [DESCPROD], [MARPROD], [PREPROD]) VALUES (8, N'Snack Veggies', N'Comida para loros', N'Nature', 3.99)
+INSERT INTO [dbo].[PRODUCTO] ([IDPROD], [NOMPROD], [DESCPROD], [MARPROD], [PREPROD]) VALUES (9, N'Nido de Bambú', N'Casa para loros', N'LivingWorld', 6.60)
+INSERT INTO [dbo].[PRODUCTO] ([IDPROD], [NOMPROD], [DESCPROD], [MARPROD], [PREPROD]) VALUES (10, N'Arnes', N'Arnes para hamster', N'Ferret', 19.99)
+SET IDENTITY_INSERT [dbo].[PRODUCTO] OFF
